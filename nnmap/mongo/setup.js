bf8d230bbfdf50f416ad3116b393d47c74ade6be
wantanblog <wantan1326@gmail.com>
2020-04-19 21:07:10 +0900

    【Docker】Nginx＋NginxUnit（Flask）＋MongoDB構成のWebアプリをDockerで再現する

---
+++ nnmap/mongo/setup.js
@@ -0,0 +1,23 @@
+const user = {
+  user: 'wantan',
+  pwd: 'wantan',
+  roles: [{
+    role: 'readWrite',
+    db: 'test'
+  }]
+};
+
+db.createUser(user);
+db.createCollection('doc');
+db.doc.insert({
+    'name' : 'wantan',
+    'num' : '1'
+});
+db.doc.insert({
+    'name' : 'ai',
+    'num' : '2'
+});
+db.doc.insert({
+    'name' : 'admin',
+    'num' : '3'
+});
